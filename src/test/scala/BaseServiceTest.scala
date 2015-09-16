91ce8d11206dbb267cc55914e8d3978769787fe5
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-09-16 10:30:22 -0700

    migrate test database

--- src/test/scala/BaseServiceTest.scala
+++ src/test/scala/BaseServiceTest.scala
@@ -2,13 +2,16 @@ import akka.actor.ActorSystem
 import akka.event.{NoLogging, LoggingAdapter}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.dddr.blackbox.http.HttpService
+import com.dddr.blackbox.utils.CouchbaseMigration
 import org.scalatest.{Matchers, FlatSpec}
 
 /**
  * Created by rroche on 9/8/15.
  */
-trait BaseServiceTest extends FlatSpec with HttpService with ScalatestRouteTest with Matchers {
+trait BaseServiceTest extends FlatSpec with HttpService with ScalatestRouteTest with Matchers with CouchbaseMigration {
   override protected val log: LoggingAdapter = NoLogging
   override implicit val system: ActorSystem
 
+  migrate()
 }
+
