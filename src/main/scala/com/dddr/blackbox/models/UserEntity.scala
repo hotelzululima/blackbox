ff8cf5b9de6f8b6c83eb6fa4334e281390b980e1
Ramón Roche <mrpollo@gmail.com>
2015-10-28 08:41:06 -0700

    akka-http bye bye

--- src/main/scala/com/dddr/blackbox/models/UserEntity.scala
+++
@@ -1,7 +0,0 @@
-package com.dddr.blackbox.models
-
-/**
- * Created by rroche on 9/8/15.
- */
-case class UserEntity(id: UserId = UserId(), login: String, password: String, name: String)
-  
--- src/main/scala/com/dddr/blackbox/utils/DroneKit.scala
+++
@@ -1,8 +0,0 @@
-package com.dddr.blackbox.utils
-
-/**
- * Created by rroche on 9/16/15.
- */
-trait DroneKit extends Config {
-
-}
