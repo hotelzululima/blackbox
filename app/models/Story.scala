01e5b644bdc7c6193ae6c2b74df658607aa2ade8
Ramón Roche <mrpollo@gmail.com>
2015-11-06 10:22:42 -0800

    javadocs for models

--- app/models/Story.scala
+++ app/models/Story.scala
@@ -8,6 +8,14 @@ import slick.driver.JdbcProfile
 /**
   * Created by rroche on 11/4/15.
   */
+
+/**
+  * Models a Story.
+  * @param id The unique id of the object.
+  * @param title Title for the story.
+  * @param created date when the Story was created.
+  * @param dronekitMedia Unique id for the DroneKit-Cloud Media object this Story belongs to.
+  */
 case class Story (id: UUID = java.util.UUID.randomUUID(),
                   title: String,
                   created: Option[Timestamp],
