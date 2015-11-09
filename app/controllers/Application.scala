a2cf45666ce867fd826c45967438857b0581ecb6
Genaro Rocha <genarorg@gmail.com>
2015-11-09 12:03:35 -0800

    * added test db config * first attempt at testing the BoxRepository

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,14 +16,14 @@ class Application @Inject() (db: BoxRepository)
                             (implicit ec: ExecutionContext) extends Controller{
 
   //test adding a box
-
+  /*
   val insertBox = db.createBox("Box Title", Some(UUID.fromString("3e536d2c-2cbb-4475-a07e-ce7776893f03")))
   insertBox onComplete{
     case Success(boxInstance) => println(boxInstance)
     case Failure(t) => println("An error occured" + t.getMessage)
 
   }
-
+  */
   def index = Action {
 
     Ok(views.html.index("Your new application is ready."))
---
+++ test/ModelSpec.scala
@@ -0,0 +1,52 @@
+
+package test
+
+import java.util.UUID
+
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import play.api.Application
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+import org.specs2.mutable.Specification
+
+import org._3dr.blackbox._
+import play.api.test.WithApplication
+
+/**
+  * Created by genarorg on 11/9/15.
+  */
+@RunWith(classOf[JUnitRunner])
+class ModelSpec extends Specification {
+
+  import models._
+
+  // -- Date helpers
+
+  def dateIs(date: java.util.Date, str: String) = new java.text.SimpleDateFormat("yyyy-MM-dd").format(date) == str
+
+  // --
+
+  "Computer model" should {
+
+    def bbRepo(implicit app: Application) = {
+      val app2BoxRepository = Application.instanceCache[BoxRepository]
+      app2BoxRepository(app)
+    }
+
+    val dronekitMissionId: UUID = UUID.fromString("4e536d2c-2cbb-4475-a07e-ce7776893f03")
+    val boxTitle = "A test box from spec"
+
+    "be retrieved by id" in new WithApplication {
+      val insertedBox: Box = Await.result(bbRepo.createBox(boxTitle, Some(dronekitMissionId)), Duration.Inf)
+      println(insertedBox)
+      insertedBox.title must equalTo(boxTitle)
+      insertedBox.dronekitMission must equalTo(dronekitMissionId)
+    }
+
+  }
+
+}
+
