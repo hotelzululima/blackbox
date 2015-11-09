db2d70ec1dbac1e6d82af1021ddf6e28812c7a0f
Ramón Roche <mrpollo@gmail.com>
2015-11-09 14:28:06 -0800

    Box model to json format

--- app/models/Box.scala
+++ app/models/Box.scala
@@ -1,7 +1,9 @@
 package models
 
 import java.sql.Timestamp
+import java.text.SimpleDateFormat
 import java.util.{Date, UUID}
+import play.api.libs.json._
 import slick.driver.JdbcProfile
 
 /**
@@ -20,5 +22,18 @@ import slick.driver.JdbcProfile
   */
 case class Box(id: UUID = java.util.UUID.randomUUID(),
                title: String,
-               created: Option[Timestamp] = Some(new Timestamp(new Date().getTime)),
+               created: Timestamp = (new Timestamp(new Date().getTime())),
                dronekitMission: Option[UUID])
+
+object Box {
+  implicit val boxFormat = Json.format[Box]
+
+  implicit object timestampFormat extends Format[Timestamp] {
+    val format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SS'Z'")
+    def reads(json: JsValue) = {
+      val str = json.as[String]
+      JsSuccess(new Timestamp(format.parse(str).getTime))
+    }
+    def writes(ts: Timestamp) = JsString(format.format(ts))
+  }
+}
